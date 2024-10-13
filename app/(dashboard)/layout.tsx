1c47bb07c31d9af5fb60f217766771c1d7029dff
Qazi Adan Hidayat <qaziadan.hidayat.skipq@gmail.com>
2024-10-13 12:47:40 +0500

    responsive

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,10 +11,10 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
 //   const isPro = (await checkSubscription()) as boolean;
   return (
     <div className={cn("h-full relative bg-white")}>
-      {/* <div className="hidden h-full md:flex md:w-64 md:flex-col md:fixed md:inset-y-0 rounded-br-lg rounded-tr-lg">
+      <div className="hidden h-full md:flex md:w-64 md:flex-col md:fixed md:inset-y-0 rounded-br-lg rounded-tr-lg">
         <Sidebar />
-      </div> */}
-      <main className={cn("")}>
+      </div>
+      <main className={cn("md:pl-64")}>
         <Navbar />
         <div className="max-h-screen rounded-tl-lg bg-white">{children}</div>
       </main>
